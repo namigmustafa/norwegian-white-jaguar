48d3a7e5f36ec7838520e17391cb6c35ecb32de8
namigmustafa <gafbil@yahoo.com>
2019-11-21 17:32:09 +0200

    Update RouteConfig.cs

--- NorwegianWhiteJaguar.UI/App_Start/RouteConfig.cs
+++ NorwegianWhiteJaguar.UI/App_Start/RouteConfig.cs
@@ -7,8 +7,8 @@ namespace NorwegianWhiteJaguar.UI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.IgnoreRoute("");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
